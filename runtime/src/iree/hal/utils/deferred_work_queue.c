66f21f8e59811c022fd7e15051294a8ba86966eb
Andrew Woloszyn <andrew.woloszyn@gmail.com>
2024-08-13 21:29:57 -0400

    Small fix
    Signed-off-by: Andrew Woloszyn <andrew.woloszyn@gmail.com>

--- runtime/src/iree/hal/utils/deferred_work_queue.c
+++ runtime/src/iree/hal/utils/deferred_work_queue.c
@@ -17,7 +17,7 @@
 #include "iree/base/internal/synchronization.h"
 #include "iree/base/internal/threading.h"
 #include "iree/hal/api.h"
-#include "iree/hal/drivers/utils/semaphore.h"
+#include "iree/hal/semaphore.h"
 #include "iree/hal/utils/deferred_command_buffer.h"
 #include "iree/hal/utils/resource_set.h"
 
@@ -1188,7 +1188,7 @@ iree_status_t iree_hal_deferred_work_queue_issue(
         if (value >= values[i]) {
           // No need to wait on this timepoint as it has already occurred and
           // we can remove it from the wait list.
-          iree_hal_semaphore_list_remove_element(&action->wait_semaphore_list,
+          iree_hal_semaphore_list_erase(&action->wait_semaphore_list,
                                                  i);
           --i;
           continue;
@@ -1225,7 +1225,7 @@ iree_status_t iree_hal_deferred_work_queue_issue(
 
         // Remove the wait timepoint as we have a corresponding event that we
         // will wait on.
-        iree_hal_semaphore_list_remove_element(&action->wait_semaphore_list, i);
+        iree_hal_semaphore_list_erase(&action->wait_semaphore_list, i);
         --i;
       }
     }
